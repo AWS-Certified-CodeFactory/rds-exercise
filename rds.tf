9117a09e324df746f92c7748844c47beda064d3d
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-08 11:38:26 +0800

    add read replica

--- rds.tf
+++ rds.tf
@@ -18,8 +18,11 @@ resource "aws_db_instance" "main" {
   allocated_storage     = 10
   max_allocated_storage = 1000
 
-  deletion_protection = false
-  skip_final_snapshot = true
+  deletion_protection      = false
+  skip_final_snapshot      = true
+  delete_automated_backups = true
+  backup_retention_period  = 7
+  apply_immediately        = true
 
   tags = local.common_tags
 }
@@ -27,6 +30,18 @@ resource "aws_db_instance" "main" {
 resource "aws_db_subnet_group" "rds_subnet" {
   name       = "${var.org}-${var.env}-rds-subnet-group"
   subnet_ids = module.vpc.private_subnets
+
+  tags = local.common_tags
+}
+
+resource "aws_db_instance" "read_replica" {
+  count               = length(module.vpc.azs)
+  identifier          = "${aws_db_instance.main.identifier}-replica-${module.vpc.azs[count.index]}"
+  replicate_source_db = aws_db_instance.main.identifier
+  instance_class      = aws_db_instance.main.instance_class
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  availability_zone      = module.vpc.azs[count.index]
 
   tags = local.common_tags
 }
